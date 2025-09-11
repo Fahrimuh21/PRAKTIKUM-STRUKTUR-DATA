da4030261ed7f754f2b6c1c323cc0902e16eed36
Muhammad Fahri <fahrimuh775@gmail.com>
2025-09-11 10:35:09 +0700

    praktikum 2

---
+++ 24060124120037_SD02/tabel.c
@@ -0,0 +1,9 @@
+#ifndef   tabel_c
+#define   tabel_c
+#include "tabel.h"
+/* Deskripsi : header file modul titik*/
+/* NIM/Nama :  24060124120037/Muhammad Fahri*/
+/* Tanggal : 11 September 2025*/
+
+
+#endif
